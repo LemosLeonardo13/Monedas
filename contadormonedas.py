89295052d55575139d982c2d8a07e5a5a1073950
LLemos <107272167+LemosLeonardo13@users.noreply.github.com>
2022-06-14 19:32:53 -0300

    Add files via upload

--- contadormonedas.py
+++ contadormonedas.py
@@ -1,33 +1,24 @@
+from cv2 import cv2
+import numpy as np
 
-from  cv2 import cv2
+valorGauss=1
+valorKernel=7
+original=cv2.imread('monedas.jpg')
+gris=cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
+gauss=cv2.GaussianBlur(gris, (valorGauss,valorGauss), 0)
+canny=cv2.Canny(gauss, 60,100)
+kernel=np.ones((valorKernel,valorKernel),np.uint8)
+cierre=cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
 
-import numpy as np #numpy me da funciones para matrices
+contornos, jerarquía=cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+print("monedas encontradas: {}".format(len(contornos)))
+cv2.drawContours(original, contornos, -1, (0,0,255),2)
+#Mostrar resultados
+cv2.imshow("Grises",gris)
+cv2.imshow("gauss",gauss)
+cv2.imshow("canny",canny)
+cv2.imshow("cierre",cierre)
 
-valorGausse=3
-ValorKernel=3
-
-Original=cv2.imread("foto\monedas.jpg")
-Grises=cv2.cvtColor(Original,cv2.COLOR_RGB2BGR)
-Gauss=cv2.GaussianBlur(Grises, (valorGausse,valorGausse),0)
-
-Canny =cv2.Canny(Original,100,200)
-
-#Kernel=np.ones((ValorKernel,ValorKernel),np.uint8)
-
-#cierre=cv2.morphologyEx(Gauss,cv2.MORPH_CLOSE, Kernel)
-
-#contorons,_=cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#print("MONEDAS ENCOTRADAS{}".format(len(contorons)))
-#cv2.drawContours(Original, contorons, -1, (0,0,255),2)
-
-#MOSTRAR RESULTADOS
-
-cv2.imshow("Grises", Grises)
-cv2.imshow("Gauss", Gauss)
-#cv2.imshow("Canny", Canny)
-#cv2.imshow("CONTORNOS", contorons)
-#cv2.imshow("Resultado", Original)
-
+cv2.imshow("Resultado", original)
 cv2.waitKey(0)
